e4fbd1328c96d7bc3ca54057ae3b6604789ba700
BatuhanKaya28 <48416741+BatuhanKaya28@users.noreply.github.com>
2019-12-25 14:03:35 +0300

    Create OrtaResultActivity.java

---
+++ OrtaResultActivity.java
@@ -0,0 +1,51 @@
+package com.example.ktbilgiyarisi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class OrtaResultActivity extends AppCompatActivity {
+    DataHelper dataHelper;
+    TextView olanpuan,eniyi,tv;
+    Button anasay;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_orta_result);
+        dataHelper = new DataHelper(this);
+
+        olanpuan = (TextView) findViewById(R.id.yapilanpuan);
+        eniyi = (TextView) findViewById(R.id.eniyiskor);
+        tv = (TextView) findViewById(R.id.tv_user);
+        anasay = (Button) findViewById(R.id.home);
+
+        int puan = dataHelper.receiveDataInt("PUAN Orta",0);
+        int eni = dataHelper.receiveDataInt("En İyi Orta",0);
+
+        olanpuan = (TextView) findViewById(R.id.yapilanpuan);
+        eniyi = (TextView) findViewById(R.id.eniyiskor);
+        tv.setText("İyi Şanslar Bir Dahaki Sefere,"+dataHelper.receiveDataString("İsim","Kullanici"));
+        olanpuan.setText(""+puan);
+
+        if(puan > eni)
+        {
+            eni = puan;
+            dataHelper.saveDataInt("En iyi Orta",eni);
+        }
+
+        eniyi.setText(""+eni);
+
+        anasay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(OrtaResultActivity.this,MainActivity.class));
+                finish();
+            }
+        });
+
+    }
+}
